abedf0f7b8bc29786998ccefdb9fb8b4141a3186
David M Anderson <daan4786@ufl.edu>
2016-11-21 08:42:37 -0500

    Added a function to determine the GC content of a DNA sequence.

--- houseelf-analysis.R
+++ houseelf-analysis.R
@@ -11,3 +11,20 @@
 
 elves_data <- read.csv("./houseel_earlength_dna_data_1.csv", stringsAsFactors = F)
 
+
+###########
+#Problem 6
+###########
+
+#1
+
+gc_content <- function(seq){
+  sequence <- str_to_upper(seq)
+  g <- str_count(sequence, "G")
+  c <- str_count(sequence, "C")
+  gc_content <- (g + c) / str_length(sequence) * 100
+  return(gc_content)
+}
+
+
+
