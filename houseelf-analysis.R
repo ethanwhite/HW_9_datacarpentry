26ed201aee8d158d994195e9351775f8b560ce86
David M Anderson <daan4786@ufl.edu>
2016-11-21 08:33:23 -0500

    Started numbering elf data sets, and changed code to reflect this

--- houseelf-analysis.R
+++ houseelf-analysis.R
@@ -1,2 +1,13 @@
 ###########
-#2
+#Problem 2
+###########
+
+#The intent of this analysis is to learn about house elves and their ears.
+
+
+###########
+#Problem 3
+###########
+
+elves_data <- read.csv("./houseel_earlength_dna_data_1.csv", stringsAsFactors = F)
+
